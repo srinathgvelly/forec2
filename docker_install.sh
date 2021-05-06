dc2d216fb289dbd0951df559bcb9bc2b13198903
srinathgvelly <76449612+srinathgvelly@users.noreply.github.com>
2021-05-06 16:38:12 +0200

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -5,6 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
-curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod 777 /var/run/docker.sock
+usermod -aG docker $USER
+curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
