1cb9074b2720563105ad69558dbc25bacdc34b8b
srinathgvelly <76449612+srinathgvelly@users.noreply.github.com>
2021-05-06 15:58:11 +0200

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
