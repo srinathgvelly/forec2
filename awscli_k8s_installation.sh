bdb430dcbaa06d37b5f0252bf3386835cb9d3492
srinathgvelly <76449612+srinathgvelly@users.noreply.github.com>
2021-05-07 15:31:34 +0200

    Update awscli_k8s_installation.sh

--- awscli_k8s_installation.sh
+++ awscli_k8s_installation.sh
@@ -1,10 +1,8 @@
 #!bin/bash
 #to install awscli
-curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
-apt install unzip python
-unzip awscli-bundle.zip
-#sudo apt-get install unzip - if you dont have unzip in your system
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 #to install kubernetes
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
